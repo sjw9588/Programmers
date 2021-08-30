64c9934030bc33d909e4400e9e5469bd8970489d
서지웅 <sjw9588@gmail.com>
2021-08-30 17:59:29 +0900

    Crane_game

--- Crane_game.java
+++ Crane_game.java
@@ -1,9 +1,18 @@
 import java.util.Stack;
 
-class Solution {
-	public int solution(int[][] board, int[] moves) {
+public class Crane_game {
+	public static void main(String[] args) {
+		int board[][] = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+		int moves[] = {1,5,3,5,1,2,1,4};
+		int answer = Solution(board,moves);
+		
+		System.out.println(answer);
+	}
+	
+	public static int Solution(int[][] board, int[] moves) {
 		int answer = 0;
-		Stack<Integer> st = new Stack();
+		Stack<Integer> st = new Stack<Integer>();
+		
 		for (int i = 0; i < moves.length; i++) { // 크레인의 이동횟수와 동일, board의 열의 개수.
 			for (int j = 0; j < board.length; j++) { // board의 행의 개수
 
@@ -22,6 +31,7 @@ class Solution {
 					board[j][moves[i]-1] = 0;
 					break;
 				}
+				else continue;	// board 배열의 해당되는 행에 인형이 없는 경우. 아무일도 일어나지 않는다.
 			}
 		}
 		return answer;
